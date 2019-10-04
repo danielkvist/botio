c798a14766b7dc430f6346c556d92d5da9fae2bc
danielkvist <danielkvist@protonmail.com>
2019-10-04 18:50:53 +0200

    update flags default values

--- cmd/server.go
+++ cmd/server.go
@@ -62,7 +62,7 @@ func serverWithBoltDB() *cobra.Command {
 	}
 
 	s.Flags().StringVar(&collection, "collection", "commands", "collection used to store commands")
-	s.Flags().StringVar(&database, "database", "./commands.db", "database path")
+	s.Flags().StringVar(&database, "database", "./botio.db", "database path")
 	s.Flags().StringVar(&porthttp, "http", ":80", "port for HTTP connections")
 	s.Flags().StringVar(&porthttps, "https", ":443", "port for HTTPS connections")
 	s.Flags().StringVar(&key, "key", "", "authentication key")
@@ -110,7 +110,7 @@ func serverWithPostgresDB() *cobra.Command {
 	s.Flags().StringVar(&host, "host", "postgres", "host of the PostgreSQL database")
 	s.Flags().StringVar(&port, "port", "5432", "port of the PostgreSQL database host")
 	s.Flags().StringVar(&database, "database", "botio", "PostgreSQL database name")
-	s.Flags().StringVar(&table, "table", "", "table of the PostgreSQL database")
+	s.Flags().StringVar(&table, "table", "commands", "table of the PostgreSQL database")
 	s.Flags().StringVar(&user, "user", "", "user of the PostgreSQL database")
 	s.Flags().StringVar(&password, "password", "", "password for the user of the PostgreSQL database")
 	s.Flags().StringVar(&porthttp, "http", ":80", "port for HTTP connections")
