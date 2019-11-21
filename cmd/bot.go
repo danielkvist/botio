d8477714e3ddd7e472fa5312f0c7b708c28720c8
Daniel <danielkvist@protonmail.com>
2019-11-21 16:09:07 +0100

    add default response flag to bot cmd

--- cmd/bot.go
+++ cmd/bot.go
@@ -16,6 +16,7 @@ func Bot() *cobra.Command {
 	var goroutines int
 	var platform string
 	var serverName string
+	var defaultResp string
 	var sslca string
 	var sslcrt string
 	var sslkey string
@@ -41,7 +42,7 @@ func Bot() *cobra.Command {
 				return errors.Wrapf(err, "while creating a new chatbot for platform %q: %v", platform, err)
 			}
 
-			b.Connect(c, u, token, goroutines)
+			b.Connect(c, u, token, goroutines, defaultResp)
 			b.Listen()
 			defer b.Stop()
 
@@ -58,6 +59,7 @@ func Bot() *cobra.Command {
 	b.Flags().IntVar(&goroutines, "goroutines", 10, "number of goroutines")
 	b.Flags().StringVar(&addr, "addr", ":9091", "botio's gRPC server address")
 	b.Flags().StringVar(&platform, "platform", "", "platform (discord or telegram)")
+	b.Flags().StringVar(&defaultResp, "resp", "I'm sorry but something's happened and I can't answer that command rigth now", "default response for when the bot fails to respond to a command")
 	b.Flags().StringVar(&sslca, "sslca", "", "ssl client certification file")
 	b.Flags().StringVar(&sslcrt, "sslcrt", "", "ssl certification file")
 	b.Flags().StringVar(&sslcrt, "sslkey", "", "ssl certification key file")
